00acffa3c53c51e33da337710e76dc521cad0635
Iago Baldani <iagobaldanialmeida@hotmail.com>
2021-11-06 17:31:54 -0300

    Ajustes gerais no Front

--- symposium/src/main/java/br/com/fatecourinhos/symposium/repository/EventoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.fatecourinhos.symposium.repository;
-
-import br.com.fatecourinhos.symposium.modelo.Evento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventoRepository extends JpaRepository<Evento, Long> {
-}
--- symposium/src/main/java/br/com/fatecourinhos/symposium/repository/OrganizadorRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.fatecourinhos.symposium.repository;
-
-import br.com.fatecourinhos.symposium.modelo.Organizador;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrganizadorRepository extends JpaRepository<Organizador, Long> {
-}
